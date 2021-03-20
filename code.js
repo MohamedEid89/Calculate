1e61c77dec34fff7ba7c14d917a773dbb56b9fe6
Mohammed Eid <info@welayer.se>
2021-03-20 03:13:33 +0100

    first commit

---
+++ code.js
@@ -0,0 +1,28 @@
+    
+
+   function solve()
+   {
+   let x = document.getElementById("edu").value
+   let y = eval(x)
+   document.getElementById("edu").value = y
+   }
+   function clr()
+   {
+   document.getElementById("edu").value = ""
+   }
+
+
+   function dis(val)
+   {
+   document.getElementById("edu").value+=val
+    }
+   function solve()
+   {
+   let x = document.getElementById("edu").value
+   let y = eval(x)
+   document.getElementById("edu").value = y
+   }
+   function clr()
+   {
+   document.getElementById("edu").value = ""
+   }
